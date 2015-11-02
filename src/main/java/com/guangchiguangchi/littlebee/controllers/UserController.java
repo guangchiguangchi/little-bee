05501ad241656c2579c08e8342bc165c02b30bde
zhangchen <kingdemoncc@qq.com>
2015-11-02 17:31:57 +0800

    add doc

--- src/main/java/com/guangchiguangchi/littlebee/controllers/UserController.java
+++ src/main/java/com/guangchiguangchi/littlebee/controllers/UserController.java
@@ -6,4 +6,31 @@ import com.jfinal.core.Controller;
  * Created by zhangchen on 15-11-2.
  */
 public class UserController extends Controller{
+    /**
+     * 新增
+     */
+    public void add(){
+
+    }
+
+    /**
+     * 修改
+     */
+    public void update(){
+
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(){
+
+    }
+
+    /**
+     * 列表
+     */
+    public void list(){
+
+    }
 }
