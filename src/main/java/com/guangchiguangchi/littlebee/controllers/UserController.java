d5501d992345fd970947cc9d22c13073531fdd22
土豆 <jwb6856@qq.com>
2015-11-03 11:58:28 +0800

    登陆接口搞定liao

--- src/main/java/com/guangchiguangchi/littlebee/controllers/UserController.java
+++ src/main/java/com/guangchiguangchi/littlebee/controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.guangchiguangchi.littlebee.controllers;
 
+import com.guangchiguangchi.littlebee.models.UserModel;
 import com.jfinal.core.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by zhangchen on 15-11-2.
@@ -32,5 +36,42 @@ public class UserController extends Controller{
      */
     public void list(){
 
+    }
+
+    /**
+     * 登录接口
+     * 参数: phonenumber 客户手机号
+     *      password 密码
+     * 返回值: 用户id
+     */
+    public void login()
+    {
+        String username=getPara("username");
+        String password=getPara("password");
+
+        String msg="";
+        Map<String,Object> data=new HashMap<>();
+        boolean success=false;
+        if("".equals(username)||username==null||"".equals(password)||password==null){
+            msg="用户名或者密码不能为空！";
+        }
+        else{
+            String sql="select m.id,m.username,m.password  from bee_users m where m.username='"+username+"' and m.password='"+password+"' ";
+            UserModel manager= UserModel.me.findFirst(sql);
+            if(manager!=null){
+                int t=manager.get("id");
+                data.put("id",String.valueOf(t));
+                success=true;
+            }
+            else
+            {
+                msg="用户名或者密码错误！请重新输入！";
+            }
+        }
+        Map<String,Object> map=new HashMap<>();
+        map.put("issuccess",success);
+        map.put("message",msg);
+        map.put("data",data);
+        renderJson(map);
     }
 }
