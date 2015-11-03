2c09d3033da75fa364bb17b53271dbeb64328b95
土豆 <jwb6856@qq.com>
2015-11-03 11:21:05 +0800

    得到用户相关的所有任务接口搞定了。。。。。。。。

--- src/main/java/com/guangchiguangchi/littlebee/controllers/TasksController.java
+++ src/main/java/com/guangchiguangchi/littlebee/controllers/TasksController.java
@@ -1,6 +1,12 @@
 package com.guangchiguangchi.littlebee.controllers;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.guangchiguangchi.littlebee.common.Uitls;
+import com.guangchiguangchi.littlebee.models.TasksModel;
 import com.jfinal.core.Controller;
+
+import java.util.List;
 
 /**
  * Created by zhangchen on 15-11-2.
@@ -32,5 +38,35 @@ public class TasksController  extends Controller {
      */
     public void list(){
 
+    }
+
+    /**
+     * 得到用户相关的所有任务
+     * 参数: id  用户id
+     */
+    public void getTaskList(){
+        String cid = getPara("id");
+        int listSize ;
+
+        List<TasksModel> creatorList = TasksModel.me.getCreatorTaskList(cid);
+        List<TasksModel> assigneeList = TasksModel.me.getAssigneeTaskList(cid);
+
+        JSONObject objJson = new JSONObject();
+
+        JSONArray creatorArrJson = new JSONArray();
+        listSize = creatorList.size();
+        for(int i = 0;i<listSize;i++){
+            creatorArrJson.add(creatorList.get(i));
+        }
+        objJson.put("creator",creatorArrJson);
+
+        JSONArray assigneeArrJson = new JSONArray();
+        listSize = assigneeList.size();
+        for(int i = 0;i<listSize;i++){
+            assigneeArrJson.add(assigneeList.get(i));
+        }
+        objJson.put("assigee",creatorArrJson);
+
+        renderJson(Uitls.Ajax.success("成功", objJson));
     }
 }
--- src/main/java/com/guangchiguangchi/littlebee/models/TasksModel.java
+++ src/main/java/com/guangchiguangchi/littlebee/models/TasksModel.java
@@ -2,9 +2,32 @@ package com.guangchiguangchi.littlebee.models;
 
 import com.jfinal.plugin.activerecord.Model;
 
+import java.util.List;
+
 /**
+ * 任务列表Model
  * Created by jiweibo on 15/11/2.
  */
 public class TasksModel extends Model<TasksModel> {
     public static final TasksModel me = new TasksModel();
+
+    /**
+     * 通过用户id 得到该用户创建任务的列表
+     * @param user_id 用户id
+     * @return
+     */
+    public List<TasksModel> getCreatorTaskList (String user_id){
+        List<TasksModel> tasklist = me.find("select * from bee_tasks where creator_id = ?",user_id);
+        return tasklist;
+    }
+
+    /**
+     * 通过用户id 得到指派给该用户的任务列表
+     * @param user_id 用户id
+     * @return
+     */
+    public List<TasksModel> getAssigneeTaskList (String user_id){
+        List<TasksModel> tasklist = me.find("select * from bee_tasks where assignee_id = ?",user_id);
+        return tasklist;
+    }
 }
