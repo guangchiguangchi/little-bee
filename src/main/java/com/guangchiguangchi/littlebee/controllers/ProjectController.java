789a469eb059df6dc3afdcfcd9e5d8af3ff4cf59
xuxinshizhao@163.com <xuxinshizhao@163.com>
2015-11-12 16:42:03 +0800

    project list

--- src/main/java/com/guangchiguangchi/littlebee/controllers/ProjectController.java
+++ src/main/java/com/guangchiguangchi/littlebee/controllers/ProjectController.java
@@ -11,97 +11,114 @@ import java.util.List;
 /**
  * Created by zhangchen on 15-11-2.
  */
-public class ProjectController  extends Controller {
+public class ProjectController extends Controller {
     /**
      * 新增
      * 项目名称：projectname
      * 项目描述：content
      * 项目状态：status
      */
-    public void add(){
-            String projectname = getPara("projectname");
-            String content = getPara("content");
+    public void add() {
+        String projectname = getPara("projectname");
+        String content = getPara("content");
 
-            if (projectname==null||content==null||projectname.trim().isEmpty()||content.trim().isEmpty()) {
-                renderJson(Uitls.Ajax.failure("项目名称和内容不能为空",""));
-                return;
-            }
-            ProjectModel project = new ProjectModel();
-            project.set("projectname",projectname);
-            project.set("content",content);
-            project.set("status",0);
+        if (projectname == null || content == null || projectname.trim().isEmpty() || content.trim().isEmpty()) {
+            renderJson(Uitls.Ajax.failure("项目名称和内容不能为空", ""));
+            return;
+        }
+        ProjectModel project = new ProjectModel();
+        project.set("projectname", projectname);
+        project.set("content", content);
+        project.set("status", 0);
 
-            project.save();
-            renderJson(Uitls.Ajax.success("添加成功",""));
+        boolean flag = project.save();
+        if (flag) {
+            renderJson(Uitls.Ajax.success("添加成功", ""));
+        } else {
+            renderJson(Uitls.Ajax.failure("添加失败", ""));
+        }
 
     }
 
     /**
      * 修改
-     *项目id： projectid
-     *项目名称： projectname
+     * 项目id： projectid
+     * 项目名称： projectname
      * 项目描述：content
      */
-    public void update(){
-            String projectid = getPara("projectid");
-            String projectname = getPara("projectname");
-            String content = getPara("content");
+    public void update() {
+        String projectid = getPara("projectid");
+        String projectname = getPara("projectname");
+        String content = getPara("content");
 
-            if (projectname==null||content==null||projectname.trim().isEmpty()||content.trim().isEmpty()) {
-                renderJson(Uitls.Ajax.failure("项目名称和内容不能为空", ""));
-                return;
-            }
-            ProjectModel project = new ProjectModel();
-            project.set("id",projectid);
-            project.set("projectname",projectname);
-            project.set("content",content);
+        if (projectname == null || content == null || projectname.trim().isEmpty() || content.trim().isEmpty()) {
+            renderJson(Uitls.Ajax.failure("项目名称和内容不能为空", ""));
+            return;
+        }
+        ProjectModel project = new ProjectModel();
+        project.set("id", projectid);
+        project.set("projectname", projectname);
+        project.set("content", content);
 
 
-            project.update();
-            renderJson(Uitls.Ajax.success("修改成功",""));
+        boolean flag = project.update();
+        if (flag) {
+            renderJson(Uitls.Ajax.success("修改成功", ""));
+        } else {
+            renderJson(Uitls.Ajax.failure("修改失败", ""));
+        }
 
     }
+
     /**
      * 修改 project 状态
      *
      * 项目id：projectid
      * 项目状态：status
      */
-    public void changeProjectStatus(){
-            String projectid = getPara("projectid");
-            String statusStr = getPara("status");
-            ProjectModel project =ProjectModel.me.findById(projectid);
+    public void changeProjectStatus() {
+        String projectid = getPara("projectid");
+        String statusStr = getPara("status");
+        ProjectModel project = ProjectModel.me.findById(projectid);
 
-            int status = 0;
-            switch (statusStr){
-                case "开始":
-                    status=1;
-                    break;
-                case "完成":
-                    status=2;
-                    break;
-                case "撤销":
-                    status=3;
-                    break;
-            }
-            project.set("status",status);
-            project.update();
-            renderJson(Uitls.Ajax.success("修改状态成功",""));
+        int status = 0;
+        switch (statusStr) {
+            case "开始":
+                status = 1;
+                break;
+            case "完成":
+                status = 2;
+                break;
+            case "撤销":
+                status = 3;
+                break;
+        }
+        project.set("status", status);
+        boolean flag = project.update();
+        if (flag) {
+            renderJson(Uitls.Ajax.success("修改状态成功", ""));
+        } else {
+            renderJson(Uitls.Ajax.failure("修改状态失败", ""));
+        }
 
     }
 
     /**
      * 删除
      *
-     *项目id： projectid
+     * 项目id： projectid
      */
-    public void delete(){
-            String projectid=getPara("projectid");
-            ProjectModel project =ProjectModel.me.findById(projectid);
+    public void delete() {
+        String projectid = getPara("projectid");
+        ProjectModel project = ProjectModel.me.findById(projectid);
 
 
-            project.delete();
-            renderJson(Uitls.Ajax.success("删除成功",""));
+        boolean flag = project.delete();
+        if (flag) {
+            renderJson(Uitls.Ajax.success("删除成功", ""));
+        } else {
+            renderJson(Uitls.Ajax.failure("删除失败", ""));
+        }
 
     }
 
@@ -110,18 +127,18 @@ public class ProjectController  extends Controller {
      *
      * 列表
      */
-    public void list(){
-            List<ProjectModel> projectList =null;
-            projectList = ProjectModel.me.find("select * from bee_projects");
-            int listSize = 0;
-            listSize = projectList.size();
-            JSONObject objJson = new JSONObject();
-            JSONArray projectArrJson = new JSONArray();
-            for(int i = 0;i<listSize;i++){
-                projectArrJson.add(projectList.get(i));
-            }
-            objJson.put("project",projectArrJson);
-            renderJson(Uitls.Ajax.success("成功",objJson));
+    public void list() {
+        List<ProjectModel> projectList = null;
+        projectList = ProjectModel.me.find("select * from bee_projects");
+        int listSize = 0;
+        listSize = projectList.size();
+        JSONObject objJson = new JSONObject();
+        JSONArray projectArrJson = new JSONArray();
+        for (int i = 0; i < listSize; i++) {
+            projectArrJson.add(projectList.get(i));
+        }
+        objJson.put("project", projectArrJson);
+        renderJson(Uitls.Ajax.success("成功", objJson));
 
     }
 }
