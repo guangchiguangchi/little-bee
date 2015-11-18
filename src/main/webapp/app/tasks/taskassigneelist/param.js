82b75a182ce0481005b007ccc41e3ca4f3439186
xuxinshizhao@163.com <xuxinshizhao@163.com>
2015-11-18 11:02:06 +0800

    任务按钮移动

--- src/main/webapp/app/tasks/taskassigneelist/param.js
+++ src/main/webapp/app/tasks/taskassigneelist/param.js
@@ -2,5 +2,10 @@
  * Created by kingcc on 15-11-10.
  */
 $f("df", "tasks.taskassigneelist.param", function (_param) {
+    switch (_param.action) {
+        case "myassign":
             return {url: "/tasks/getAssigneeTaskList", data: {userid: 1}, target: _param.target};
+            break;
+    }
+
 });
