7549babcc4070c3ba89dda7dcfa5cc5dedf5ade9
user-avatar 鲍友嘉 <193096510@qq.com>
2015-11-18 16:24:14 +0800

    测试

--- src/main/webapp/app/tasks/taskassigneeedit/combine.js
+++ src/main/webapp/app/tasks/taskassigneeedit/combine.js
@@ -5,6 +5,7 @@
 $f("df","tasks.taskassigneeedit.combine",function(_temp,_resp,_scope){
     var render = template.compile(_temp);
     var html = render(_resp);
+    $('#myModal').empty();
     $("#" + _scope.target)[0].innerHTML=html;
     $('#myModal').modal();
     return;
