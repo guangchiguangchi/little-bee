43f1fbd439be2746c411f570ea1af7457f9b5f6a
user-avatar 鲍友嘉 <193096510@qq.com>
2015-11-18 14:38:08 +0800

    前端日志界面

---
+++ src/main/webapp/app/tasks/logslist/param.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kingcc on 15-11-10.
+ */
+$f("df", "tasks.tasklist.param", function (_param) {
+    switch (_param.action) {
+        case "logslist":
+            return {url: "logs/list", data: {}, target: _param.target};
+            break;
+    }
+});
