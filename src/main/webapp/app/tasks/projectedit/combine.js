a69a5ad0e493738492f30975c7f4bbbf1367e875
zhangchen <kingdemoncc@qq.com>
2015-11-19 14:18:21 +0800

    多个弹框

--- src/main/webapp/app/tasks/projectedit/combine.js
+++ src/main/webapp/app/tasks/projectedit/combine.js
@@ -5,7 +5,7 @@
 $f("df","tasks.projectedit.combine",function(_temp,_resp,_scope){
     var render = template.compile(_temp);
     var html = render(_resp);
-    $("#" + _scope.target).html(html);
+    $("#" + _scope.target)[0].innerHTML=html;
     $('#myModal').modal();
     return;
 });
