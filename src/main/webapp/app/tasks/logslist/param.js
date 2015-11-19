9d36a2a96ef801e966530a6ebca0493c2484e201
zhangchen <kingdemoncc@qq.com>
2015-11-19 09:29:56 +0800

    细节调整

--- src/main/webapp/app/tasks/logslist/param.js
+++ src/main/webapp/app/tasks/logslist/param.js
@@ -4,7 +4,7 @@
 $f("df", "tasks.logslist.param", function (_param) {
     switch (_param.action) {
         case "logslist":
-            return {url: "logs/list", data: {}, target: _param.target};
+            return {url: "logs/list", data: {"uid":_param.uid}, target: _param.target};
             break;
     }
 });
