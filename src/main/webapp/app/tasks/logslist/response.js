e79ae21ffedceec4f97616a69570bd97c60fe45d
xuxinshizhao@163.com <xuxinshizhao@163.com>
2015-11-19 12:03:06 +0800

    html 修改

--- src/main/webapp/app/tasks/logslist/response.js
+++ src/main/webapp/app/tasks/logslist/response.js
@@ -1,6 +1,6 @@
 /**
- * Created by kingcc on 15-11-10.
- */
+* Created by kingcc on 15-11-10.
+*/
 $f("df","tasks.logslist.response",function(_data,_scope){
     if(_data.issuccess){
         return _data.data;
--- src/main/webapp/app/tasks/projectedit/projectstatus.js
+++ src/main/webapp/app/tasks/projectedit/projectstatus.js
@@ -2,11 +2,6 @@
  * Created by smp on 2015/11/17.
  */
 $f("df","tasks.projectedit.projectstatus",function(_obj){
-<<<<<<< HEAD
-    var _data = {id:_obj.value,status:_obj.name}
-=======
-    console.log(_obj.name  + ":" + _obj.value);
     var _data = {id:_obj.value,status:_obj.name,uid:window.location.href.split('uid=')[1]}
->>>>>>> 72966260435edc55fab6eace3eee218cf7c39fea
     $f('SendData',_data,'tasks.projectedit.changestatusreq','tasks.projectedit.changestatusresp');
 })
